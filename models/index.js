d2d0490ef93de4b8d8eea531f79799cdb5eaa4f5
Yosi Arbiv <yosi1a1@outlook.com>
2022-03-05 18:58:24 +0200

    ♻️ changes db configuration

--- models/index.js
+++ models/index.js
@@ -1,15 +1,12 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "wjkzlytlbrkzbe",
-  host: "ec2-176-34-105-15.eu-west-1.compute.amazonaws.com",
-  database: "dbiog8i57pkf3",
-  password: "0b90f9d5de0e2c919c89170e8de85a4e5e427e8568a7039cd5fb4514a003ca72",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
 
 const executeTransaction = async (callback) => {
   const client = await pool.connect();
