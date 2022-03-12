4e014eb28efc910c50c72fe2b9d01a599237cec9
Shir Agami <shirag2007@gmail.com>
2022-03-12 15:42:58 +0200

    ✨ fix json response on join request

--- routes/api/joinRouter.js
+++ routes/api/joinRouter.js
@@ -7,7 +7,7 @@ router.post('/', authenticateToken, isUserEligibleToJoin, async (req, res) => {
     try {
         //req.group is received after isUserEligibleToJoin and req.user is received after authenticateToken
         await GroupService.addGroupMembers(req.group.id, [req.user.id]);
-        res.sendStatus(200);
+        res.json(req.group);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
